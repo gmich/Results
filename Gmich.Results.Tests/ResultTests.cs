a01553d1610ed997d79c4f3c23028633ab96558b
George Michaelides <g.mich90@gmail.com>
2016-06-10 11:37:32 +0300

    Added some unit tests

---
+++ Gmich.Results.Tests/ResultTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Gmich.Results.Tests
+{
+    [TestClass]
+    public class ResultTests
+    {
+        private Result SuccessfulResult => Result.Ok();
+        private Result FailedResult => Result.FailWith(State.Error, "Failed Result");
+
+        [TestMethod]
+        public void SuccessfulResult_Test()
+        {
+            SuccessfulResult
+            .OnSuccess(res =>
+                Assert.IsTrue(res.Success))
+            .OnFailure(res =>
+                Assert.Fail("This should be successful"));
+        }
+
+        [TestMethod]
+        public void FailedResult_Test()
+        {
+            SuccessfulResult
+            .OnSuccess(res =>
+                Assert.Fail("This should fail"))
+            .OnFailure(res =>
+                Assert.IsTrue(res.Failure));
+        }
+    }
+}
